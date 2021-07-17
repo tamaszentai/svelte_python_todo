10ad727c8de7fab3a28aaaa412fba8d4fa2bc284
Tamas Zentai <zentaitamas@gmail.com>
2021-07-17 15:52:32 +0100

    add database

--- server/app.py
+++ server/app.py
@@ -5,10 +5,34 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+#I Init marshmallow
+ma = Marshmallow(app)
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({ 'msg': 'Hello World'})
+# Todo Class/Model
+class Todo(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String(100))
+    isCompleted = db.Column(db.Boolean)
+
+    def __init__(self, id, name, isCompleted):
+        self.id = id
+        self.name = name
+        self.isCompleted = isCompleted
+
+# Todo Schema
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'isCompleted')
+
+# Init Schema
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
 
 # Run server
 if __name__ == '__main__':
